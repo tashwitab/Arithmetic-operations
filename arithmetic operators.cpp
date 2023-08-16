e18febb7e561525c3705bcf1e7446e0b946752d0
tashwitab <142425346+tashwitab@users.noreply.github.com>
2023-08-16 20:34:17 +0530

    Add files via upload

---
+++ arithmetic operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    num1=70;
+    num2=12;
+cout<< "num1+num2 = " <<(num1+num2)<<endl;
+cout<< "num1-num2 = "<<(num1-num2)<<endl;
+cout<< "num1*num2 = "<<(num1*num2)<<endl;
+cout<< "num1/num2 = "<<(num1/num2)<<endl;
+cout<< "num1%num2 = "<<(num1%num2)<<endl;
+
+}
